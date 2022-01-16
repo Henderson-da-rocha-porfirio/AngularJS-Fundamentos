90f860e36b8af35141d4ac55add8066a54dff0b1
Henderson <hendersonporfirio@protonmail.com>
2022-01-16 11:39:27 -0300

    #0.4_commit_alteracoes_2

--- application.js
+++ application.js
@@ -1,14 +1,4 @@
-var app = angular.module('app', []); // controllers foi injetado como parametro para ser recebido no modulo abaixo.
-/* var app = angular.module('app', ['controllers']); // controllers foi injetado como parametro para ser recebido no modulo abaixo. */
-
-/* var MainController = function ($scope) {
-    $scope.val = "testes" */
-    /* $scope.func = function () {
-        return "123" + "456"
-    }     
-} */
-
-/* var app = angular.module('app', []);
+var app = angular.module('app', []);
 
 app.controller('MainController', function($scope) {
   $scope.val = 1
@@ -18,17 +8,4 @@ app.controller('MainController', function($scope) {
     $scope.val += 1
     $scope.even = $scope.val%2==0
   }
-}) */
-
-var app = angular.module('app', []);
-
-app.controller('MainController', function($scope) {
-  // $scope.myarr = [1,2,3,4,5,5,6,7,8,9]
-
-  // $scope.users = [{name:"mike",age:23},{name:"andy",age:34},{name:"reid",age:45},{name:"reid",age:45}]
-
-  $scope.ob = {name: "mike", age:35, title:"Mr."}
 })
-
-
-   
