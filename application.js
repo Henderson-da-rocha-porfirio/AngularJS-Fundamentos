391368e6debb1a09cc21e801727df2adbbdae5a0
Henderson <hendersonporfirio@protonmail.com>
2022-01-18 21:44:57 -0300

    commit

--- application.js
+++ application.js
@@ -1,5 +1,7 @@
 var app = angular.module('app', []);
 
 app.controller('MainController', function ($scope) {
-    $scope.mydata = {arr: [{name:"joao",age:34},{name:"Filipo",age:54}]}
+    $scope.mydata = {arr: [{name:"joao",age:34},{name:"Filipo",age:54},{name:"Marco",age:14},{name:"Brigite",age:87}]}
+
+    /* console.log($filter('uppercase')('make this string uppercase!')) */
 })
