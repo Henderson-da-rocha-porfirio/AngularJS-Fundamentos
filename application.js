129f327ad0293e9d9a9e089e87dd52ee9cc3d211
Henderson <hendersonporfirio@protonmail.com>
2022-01-18 22:15:32 -0300

    commit

--- application.js
+++ application.js
@@ -1,7 +1,41 @@
 var app = angular.module('app', []);
 
 app.controller('MainController', function ($scope) {
-    $scope.mydata = {arr: [{name:"joao",age:34},{name:"Filipo",age:54},{name:"Marco",age:14},{name:"Brigite",age:87}]}
+    $scope.mydata = { arr: [{ name: "joao", age: 34 }, { name: "Filipo", age: 54 }, { name: "Marco", age: 14 }, { name: "Brigite", age: 87 }] }
 
     /* console.log($filter('uppercase')('make this string uppercase!')) */
+})
+
+/* app.filter('charlimit', function () {
+    return function(input, length) {
+        if (!length) {
+            length = 10
+        }
+        if (!input) {
+            return ''
+        }
+        if (input.length <= length) {
+            return input
+        } else {
+            return input.substring(0, length) + '...'
+        }
+    }
+}) */
+
+app.filter('podeBeber', function () {
+    return function (data, idadeMinima) {
+        var filtered = [];
+        if (!idadeMinima) {
+            idadeMinima = 21
+        }
+        for (var i = 0; i < data.length; i++) {
+            var value = data[i];
+            if (value.age >= idadeMinima) {
+                filtered.push(value);
+            }
+        }
+
+        return filtered;
+
+    }
 })
