035d75f596bd4a941b430d9dc49bf09c25be306a
Henderson <hendersonporfirio@protonmail.com>
2022-01-22 15:22:35 -0300

    commit

--- application.js
+++ application.js
@@ -1,11 +1,25 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function($scope,twitterAPI,valService) {
-  // console.log(constService,valService())
-  twitterAPI.url = "newurl"
-  console.log(twitterAPI)
+app.controller('MainController', function($scope,myFactory,Auth) {
+  console.log(myFactory.getdata())
+  console.log(myFactory.mydata)
 })
 
-app.constant('twitterAPI', {url: "api.twitter.com/v1/"})
+app.factory('myFactory',function() {
+  var mydata = "this is some other data"
+  var myfunc = function() {}
+  return {
+    getdata: function() {
+      return mydata
+    }  
+  }
+})
 
-app.value('valService', function() {return "this is returned from a fn"}) 
+app.factory('Auth',function() {
+  var current_user = {}
+  return {
+    setUser: function() {},
+    login: function() {}
+    logout:
+  }
+})
