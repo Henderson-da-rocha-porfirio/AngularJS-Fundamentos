55dd7534715e5996d2230fa8afb5f9672e4aebb8
Henderson <hendersonporfirio@protonmail.com>
2022-01-22 17:38:31 -0300

    commit

--- application.js
+++ application.js
@@ -5,40 +5,33 @@ app.controller('MainController', function($scope) {
                   date:"13 de Janeiro, 2022."}
 })
 
-app.directive('photo', function() {
-  return {
-    restrict: 'E',
-
-    // Directives types:
-    // E = element
-    // A = attribute
-    // C = class 
-    // M = constant
-    // Exemplo de uma directive que pode tomar várias formas = restrict: 'AE'
-
-    template: '<figure><img width="500px"/><figcaption/></figure>',
-    replace: true,
-
-    link: function(scope, element, attrs) {
-      attrs.$observe('caption', function(value) {
-        element.find('figcaption').text(value)
-      })
-
-      attrs.$observe('photoSrc', function(value) {
-        element.find('img').attr('src', value)
-      })
-    }
-  }
-})
-
 // app.directive('photo', function() {
-//     return {
-//         restrict: 'E',
-//         templateUrl: "photo.html",
-//         replace: true,
-//         scope: {
-//             caption: '@',
-//             photoSrc: '@'
-//         }
+//   return {
+//     restrict: 'E',
+
+//     template: '<figure><img width="500px"/><figcaption/></figure>',
+//     replace: true,
+
+//     link: function(scope, element, attrs) {
+//       attrs.$observe('caption', function(value) {
+//         element.find('figcaption').text(value)
+//       })
+
+//       attrs.$observe('photoSrc', function(value) {
+//         element.find('img').attr('src', value)
+//       })
 //     }
+//   }
 // })
+
+app.directive('photo', function() {
+    return {
+        restrict: 'E',
+        templateUrl: "photo.html",
+        replace: true,
+        scope: {
+            caption: '@capstring',
+            photoSrc: '='
+        }
+    }
+})
