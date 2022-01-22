83a8c755a09e327e9ff8fb3c9a592bc924d813fa
Henderson <hendersonporfirio@protonmail.com>
2022-01-22 15:01:31 -0300

    Commit

--- application.js
+++ application.js
@@ -1,41 +1,11 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function ($scope) {
-    $scope.mydata = { arr: [{ name: "joao", age: 34 }, { name: "Filipo", age: 54 }, { name: "Marco", age: 14 }, { name: "Brigite", age: 87 }] }
-
-    /* console.log($filter('uppercase')('make this string uppercase!')) */
+app.controller('MainController', function($scope,twitterAPI,valService) {
+  // console.log(constService,valService())
+  twitterAPI.url = "newurl"
+  console.log(twitterAPI)
 })
 
-/* app.filter('charlimit', function () {
-    return function(input, length) {
-        if (!length) {
-            length = 10
-        }
-        if (!input) {
-            return ''
-        }
-        if (input.length <= length) {
-            return input
-        } else {
-            return input.substring(0, length) + '...'
-        }
-    }
-}) */
+app.constant('twitterAPI', {url: "api.twitter.com/v1/"})
 
-app.filter('podeBeber', function () {
-    return function (data, idadeMinima) {
-        var filtered = [];
-        if (!idadeMinima) {
-            idadeMinima = 21
-        }
-        for (var i = 0; i < data.length; i++) {
-            var value = data[i];
-            if (value.age >= idadeMinima) {
-                filtered.push(value);
-            }
-        }
-
-        return filtered;
-
-    }
-})
+app.value('valService', function() {return "this is returned from a fn"})
