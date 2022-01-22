a2841099462e72978c72895f5bda719a8c942db6
Henderson <hendersonporfirio@protonmail.com>
2022-01-22 15:24:48 -0300

    commit

--- application.js
+++ application.js
@@ -1,25 +1,53 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function($scope,myFactory,Auth) {
-  console.log(myFactory.getdata())
-  console.log(myFactory.mydata)
+app.controller('MainController', function($scope,myFactory,myService) {
+  // console.log(myFactory.getData())
+  // myFactory.addData('bla bla bla')
+  // console.log(myFactory.getData())
+  console.log(myService.getData())
+  myService.addData('bla bla bla')
+  console.log(myService.getData())
 })
 
 app.factory('myFactory',function() {
-  var mydata = "this is some other data"
-  var myfunc = function() {}
+  var myString = "this is some other data"
+  var addToString = function(newstr) {
+    myString += newstr
+  }
   return {
-    getdata: function() {
-      return mydata
-    }  
+    getData: function() {
+      return "String contains: " + myString
+    },
+    addData: addToString
   }
 })
 
-app.factory('Auth',function() {
-  var current_user = {}
-  return {
-    setUser: function() {},
-    login: function() {}
-    logout:
+// app.service('myService',function() {
+//   var myString = "this is some other data"
+//   var addToString = function(newstr) {
+//     myString += newstr
+//   }
+
+//   this.getData = function() {
+//     return "String contains: " + myString
+//   }
+//   this.addData = addToString
+// })
+
+// app.factory('myService',function(){
+//   return new ServiceClass();
+// })
+
+app.service('myService',ServiceClass)
+
+function ServiceClass() {
+  var myString = "this is some other data"
+  var addToString = function(newstr) {
+    myString += newstr
   }
-})
+
+  this.getData = function() {
+    return "String contains: " + myString
+  }
+  this.addData = addToString
+}
