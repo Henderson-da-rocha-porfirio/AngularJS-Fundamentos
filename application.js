f02df1a6d9ced59d6fb8dd4abe8b4aae66e24ef0
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 07:36:14 -0300

    commit

--- application.js
+++ application.js
@@ -1,32 +1,27 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function ($scope) {
-    $scope.photo = {
-        url: "https://cdn.pixabay.com/photo/2022/01/13/14/02/mother-6935336_960_720.jpg",
-        date: "13 de Janeiro, 2022."
-    }
+app.controller('MainController', function($scope) {
 
-    $scope.callme = function (msg) {
-        console.log('Você me deu: ' + msg)
-    }
 })
 
-app.directive('photo', function () {
-    return {
-        restrict: 'E',
-        templateUrl: "photo.html",
-        replace: true,
-        scope: {
-            caption: '@capstring',
-            // O '@capstring' está aceitando dados dentro da string passada em: Mother - Imagem carregada no Pixabay em: {{photo.date}}
-            // A 'string' é tratada dentro do dado interpolado ({{}})
-            // 'caption' está presente no escopo da diretiva
-            // Quando 'caption' é escrita com o scope da diretiva, isso não sobrescreve o scope pai. Contudo, a photo (origem) que está vinculada a instância de photo pai será sobrescrita.
-            photoSrc: '=', // '=' pega a ligação do que for passado para ele como uma string e continua essa ligação no escopo da diretiva
-            localFunc: '&' // '&' 'e comercial'(ampersand em inglês) significa que você quer passar uma expressão do alto nível(controller pai) para o nível baixo(nível da diretiva) por isso o controller abaixo.
-        },
-        controller: function ($scope) { //Uma diretiva controller para atribuir dados ao scope. Isso funciona conforme a explicação acima sobre o '&'. E usa a injeção de dependência($scope)
-            $scope.mydata = "some data"
-        }
+app.directive('row', function() {
+  return {
+    restrict: 'E',
+    compile: function(tElement, attrs) {
+      var content = angular.element('<div class="row"></div>')
+      content.append(tElement.contents());
+      tElement.replaceWith(content);
     }
-})
+  }
+});
+
+app.directive('column', function() {
+  return {
+    restrict: 'E',
+    compile: function(tElement, attrs) {
+      var content = angular.element('<div class="col"></div>')
+      content.append(tElement.contents());
+      tElement.replaceWith(content);
+    }
+  }
+});
