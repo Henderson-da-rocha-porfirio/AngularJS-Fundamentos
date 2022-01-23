c80b587a1c9c1ecefd36ef957b1f034d0fd97db3
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 09:58:37 -0300

    commit

--- application.js
+++ application.js
@@ -1,10 +1,14 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.controller('MainController', function($scope) {
-  $scope.somedata = "This is some data!"
+app.controller('MainController', function($scope,$routeParams) {
+
+  console.log($routeParams)
+  $scope.somedata = $routeParams.myparam
 });
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/', {templateUrl: 'view.html'});
+    .when('/', {templateUrl: 'view.html'})
+    .when('/test/:myparam/:myparam2/:myparam3', {templateUrl: 'view2.html'})
+    .otherwise({template: 'Couldn\'t match a route'})
 })
