ccf205380be0758a74b741c3fa83ab8afa3670fc
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 07:53:36 -0300

    commit

--- application.js
+++ application.js
@@ -4,24 +4,54 @@ app.controller('MainController', function($scope) {
 
 })
 
-app.directive('row', function() {
+app.directive('marines', function() {
   return {
-    restrict: 'E',
-    compile: function(tElement, attrs) {
-      var content = angular.element('<div class="row"></div>')
-      content.append(tElement.contents());
-      tElement.replaceWith(content);
+    restrict: 'A',
+    controller: function() {
+      this.soundoff = function() {
+        console.log("Marine Corps")
+      }
     }
   }
-});
+})
 
-app.directive('column', function() {
+app.directive('gunnerySergeant', function() {
   return {
-    restrict: 'E',
-    compile: function(tElement, attrs) {
-      var content = angular.element('<div class="col"></div>')
-      content.append(tElement.contents());
-      tElement.replaceWith(content);
+    restrict: 'A',
+    controller: function($scope) {
+      $scope.soundoff = function() {
+        console.log("Gunnery Sgt.")
+      }
+    },
+    require: ["^soldier","^marines","^?medalOfHonor"],
+    link: function (scope, element, attrs, ctrls) {
+      scope.soundoff()
+      angular.forEach(ctrls, function(ctrl) {
+        if (!!ctrl) { ctrl.soundoff() }
+      })
     }
   }
-});
+})
+
+app.directive('soldier', function() {
+  return {
+    restrict: 'E',
+    controller: function() {
+      this.soundoff = function() {
+        console.log("Soldier")
+      }
+    }
+  }
+})
+
+app.directive('medalOfHonor', function() {
+  return {
+    restrict: 'A',
+    controller: function() {
+      this.soundoff = function() {
+        console.log("Medal Of Honor")
+      }
+    }
+  }
+})
+
