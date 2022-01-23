a0ef5bbb6649fd005bb03bdabfb1e6fe1dc4bedf
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 09:28:26 -0300

    commit

--- application.js
+++ application.js
@@ -1,5 +1,10 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
 app.controller('MainController', function($scope) {
-
+  $scope.somedata = "This is some data!"
 });
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {templateUrl: 'view.html'});
+})
