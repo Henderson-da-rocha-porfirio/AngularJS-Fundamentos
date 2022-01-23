fd16991342fee857c10d2b84d14b58ca7393cb1d
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 11:10:42 -0300

    commit

--- application.js
+++ application.js
@@ -1,34 +1,16 @@
 var app = angular.module('app', ['ngRoute']);
 
-var mainCtrl = app.controller('MainController', function($scope) {
-  $scope.somedata = "This is some data!"
+app.controller('MainController', function($scope,$routeParams) {
+  $scope.somedata = $routeParams.myparam
 });
-
-mainCtrl.data2 = function($q,$timeout) {
-            var defer = $q.defer();
-            $timeout(function() {
-              defer.resolve();
-              console.log('also finished')
-            }, 1000);
-            return defer.promise;
-          }
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/', 
-      {
-        templateUrl: 'view.html',
-        resolve: {
-          data1: function($q,$timeout) {
-            var defer = $q.defer();
-            $timeout(function() {
-              defer.resolve();
-              console.log('finished')
-            }, 2000);
-            return defer.promise;
-          },
-          data2: mainCtrl.data2
-        }
-      })
+    .when('/', {templateUrl: 'view.html'})
+    .when('/test', {redirectTo: function(routeParams,path,search) {
+      console.log(routeParams,path,search)
+      return '/test/'+search.mynewparam
+    }})
+    .when('/test/:myparam', {templateUrl: 'view2.html'})
     .otherwise({template: 'Couldn\'t match a route'})
 })
