612a0956eadd2405599f19953b2c4ac9eecb140d
Henderson <hendersonporfirio@protonmail.com>
2022-01-23 08:20:17 -0300

    commit

--- application.js
+++ application.js
@@ -2,12 +2,4 @@ var app = angular.module('app', []);
 
 app.controller('MainController', function($scope) {
 
-})
-
-app.directive('foo', function() {
-  return {
-    restrict: 'E',
-    transclude: true,
-    template: '<div>This is the template content!</div><div ng-transclude></div>'
-  }
-})
+});
