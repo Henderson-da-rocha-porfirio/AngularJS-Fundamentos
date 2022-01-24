f4cc8be3f3988c255f4388a5e93afa9464b7d680
Henderson <hendersonporfirio@protonmail.com>
2022-01-24 14:27:34 -0300

    commit

--- application.js
+++ application.js
@@ -1,31 +1,69 @@
-var myapp = angular.module('myapp', ["ui.router"])
-myapp.config(function($stateProvider, $urlRouterProvider){
+var app = angular.module("myApp", [
+  "ngRoute",
+  "ngAnimate",
+  ]);
   
-  // For any unmatched url, send to /route1
-  $urlRouterProvider.otherwise("/route1")
   
-  $stateProvider
-    .state('route1', {
-        url: "/route1",
-        templateUrl: "route1.html"
-    })
-      .state('route1.list', {
-          url: "/list",
-          templateUrl: "route1.list.html",
-          controller: function($scope){
-            $scope.items = ["A", "List", "Of", "Items"];
-          }
-      })
-      
-    .state('route2', {
-        url: "/route2",
-        templateUrl: "route2.html"
-    })
-      .state('route2.list', {
-          url: "/list",
-          templateUrl: "route2.list.html",
-          controller: function($scope){
-            $scope.things = ["A", "Set", "Of", "Things"];
-          }
-      })
-})
+  
+  app.config(function($routeProvider, $locationProvider) {
+  
+    $routeProvider.
+      when("/first-page", {
+        templateUrl: "first-page.html",
+        animate: "slideup"
+      }).
+      when("/second-page", {
+        templateUrl: "second-page.html",
+        animate: "slideup"
+      }).
+      otherwise({
+        redirectTo: "/first-page"
+      });
+  
+  });
+  
+  app.controller("MyCtrl", function($scope) {
+  
+  });
+  
+  // PAGE ANIMATE DIRECTIVE
+  app.directive('animClass',function($route){
+    return {
+      link: function(scope, elm, attrs){
+        var enterClass = $route.current.animate;
+        elm.addClass(enterClass)
+        scope.$on('$destroy',function(){
+          elm.removeClass(enterClass)
+          elm.addClass($route.current.animate)
+        })
+       }
+    }
+  });
+  
+  // PARALLAX DIRECTIVE
+  app.directive('parallaxBackground', ['$window', function($window) {
+    return {
+      restrict: 'A',
+      transclude: true,
+      template: '<div ng-transclude></div>',
+      scope: {
+        parallaxRatio: '@',
+      },
+      link: function($scope, elem, attrs) {
+        var setPosition = function () {
+          var calcValY = (elem.prop('offsetTop') - $window.pageYOffset) * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1 );
+          // horizontal positioning
+          elem.css('background-position', "50% " + calcValY + "px");
+         };
+  
+        // set our initial position - fixes webkit background render bug
+        angular.element($window).bind('load', function(e) {
+          setPosition();
+          $scope.$apply();
+        });
+  
+        angular.element($window).bind("scroll", setPosition);
+        angular.element($window).bind("touchmove", setPosition);
+      }  // link function
+    };
+  }]);
